bb696afffa70f54cfc6cce70a472844c9552a257
MohamedHassan010 <mohammed20191700522@cis.asu.edu.eg>
2022-04-28 16:59:12 +0200

    Login From

---
+++ Auction Management System/Form1.Designer.cs
@@ -0,0 +1,137 @@
+﻿
+namespace Auction_Management_System
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.EmailText = new System.Windows.Forms.TextBox();
+            this.PasswordText = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.LoginButton = new System.Windows.Forms.Button();
+            this.RegisterButton = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(179, 156);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(42, 17);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Email";
+            // 
+            // EmailText
+            // 
+            this.EmailText.Location = new System.Drawing.Point(282, 153);
+            this.EmailText.Name = "EmailText";
+            this.EmailText.Size = new System.Drawing.Size(239, 22);
+            this.EmailText.TabIndex = 1;
+            this.EmailText.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // PasswordText
+            // 
+            this.PasswordText.Location = new System.Drawing.Point(282, 236);
+            this.PasswordText.Name = "PasswordText";
+            this.PasswordText.Size = new System.Drawing.Size(239, 22);
+            this.PasswordText.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(152, 250);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(69, 17);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Password";
+            // 
+            // LoginButton
+            // 
+            this.LoginButton.Location = new System.Drawing.Point(282, 298);
+            this.LoginButton.Name = "LoginButton";
+            this.LoginButton.Size = new System.Drawing.Size(239, 51);
+            this.LoginButton.TabIndex = 4;
+            this.LoginButton.Text = "Login";
+            this.LoginButton.UseVisualStyleBackColor = true;
+            this.LoginButton.Click += new System.EventHandler(this.LoginButton_Click);
+            // 
+            // RegisterButton
+            // 
+            this.RegisterButton.Location = new System.Drawing.Point(282, 378);
+            this.RegisterButton.Name = "RegisterButton";
+            this.RegisterButton.Size = new System.Drawing.Size(245, 51);
+            this.RegisterButton.TabIndex = 5;
+            this.RegisterButton.Text = "Register";
+            this.RegisterButton.UseVisualStyleBackColor = true;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::Auction_Management_System.Properties.Resources.img_3375311;
+            this.pictureBox1.Location = new System.Drawing.Point(309, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(201, 106);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 6;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.RegisterButton);
+            this.Controls.Add(this.LoginButton);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.PasswordText);
+            this.Controls.Add(this.EmailText);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox EmailText;
+        private System.Windows.Forms.TextBox PasswordText;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button LoginButton;
+        private System.Windows.Forms.Button RegisterButton;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ Auction Management System/Form1.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Oracle.DataAccess.Client;
+using Oracle.DataAccess.Types;
+namespace Auction_Management_System
+{
+    
+    public partial class Form1 : Form
+    {
+        string ordb = "Data Source=orcl; User Id=hr;Password=hr;";
+        OracleConnection conn;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void LoginButton_Click(object sender, EventArgs e)
+        {
+            conn = new OracleConnection(ordb);
+            conn.Open();
+            OracleCommand cmd = new OracleCommand();
+            cmd.Connection = conn;
+            cmd.CommandText = "select customerid  from customer where customeremail=:email and customerpassword=:password";
+            cmd.CommandType = CommandType.Text;
+            cmd.Parameters.Add("email", EmailText.Text);
+            cmd.Parameters.Add("password", PasswordText.Text);
+            OracleDataReader dataReader = cmd.ExecuteReader();
+            if (dataReader.Read())
+            {
+                MessageBox.Show("Success");
+            }
+        }
+    }
+}
